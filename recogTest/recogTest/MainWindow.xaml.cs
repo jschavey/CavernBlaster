db00dcefbf3c12cd84f47f80752885049d2df661
Mike Rand <mikerand@tknw.com>
2019-10-21 16:58:55 -0700

    working?

---
+++ recogTest/recogTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace recogTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BitmapSource Capture()
+        {
+            using (var snipitSize = new Bitmap(100, 100, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using(var snipit = Graphics.FromImage(snipitSize))
+                {
+                    snipit.CopyFromScreen(0, 0, 0, 0, snipitSize.Size);
+                    return Imaging.CreateBitmapSourceFromHBitmap(snipitSize.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            bool isObjectPresent = false;
+            
+            image.Source = Capture();
+        }
+
+        private void STOP(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
